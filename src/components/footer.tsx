c231c0766a923b5d822d244b3138cbee81d4778d
jagotranaveen <jagotra.naveen@gmail.com>
2024-09-22 17:10:49 +0530

    First Commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import { Card, Grid, Heading, Text } from "@radix-ui/themes";
+
+export function Footer() {
+  return (
+    <Grid columns={{ initial: "1", sm: "3" }} gap={{ initial: "3", sm: "5" }}>
+        HSP Group 
+    </Grid>
+  );
+}
